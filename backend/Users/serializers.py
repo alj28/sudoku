a4aca679a33c0f1210434e0dd0c5f8e6f7520ffa
Aljaz K <krasna.aljaz@gmail.com>
2025-02-14 15:00:19 +0100

    Re-worked Users serializer.py

--- backend/Users/serializers.py
+++ backend/Users/serializers.py
@@ -1,7 +1,8 @@
 
-from rest_framework import serializers, status
-from password_strength import PasswordPolicy
+from dataclasses import dataclass
 from django.contrib.auth.models import User
+from password_strength import PasswordPolicy
+from rest_framework import serializers, status
 from django.contrib.auth.hashers import check_password
 
 PASSWORD_CHARFIELD_ARGS = {
@@ -11,7 +12,7 @@ PASSWORD_CHARFIELD_ARGS = {
     }
 }
 
-def password_policy_check(password: str):
+def is_password_strong(password: str):
     policy = PasswordPolicy.from_names(
         length=8,
         uppercase=2,
@@ -22,7 +23,44 @@ def password_policy_check(password: str):
     # PasswordPolicy does not support definition for minimum number of lower-case characters
     n_lower_case = len([c for c in password if c.islower()])
     if 0 != len(policy.test(password)) or n_lower_case < 2:
+        return False
+    return True
+
+def password_policy_check(password: str):
+    if False == is_password_strong(password):
         raise serializers.ValidationError('Password too weak', code=status.HTTP_400_BAD_REQUEST)
+    
+def add_error_to_dict(key: str, errors_dict: dict, msg: str):
+    if key not in errors_dict:
+        errors_dict[key] = []
+    errors_dict[key].append(msg)
+@dataclass
+class NewPasswordValidator:
+    username: str
+    email: str
+    password_1: str
+    password_2: str
+    
+    def validate(self, errors_state: dict):
+        if self.password_2 != self.password_1:
+            add_error_to_dict('password_2', errors_state, 'Passwords do not match')
+        if self.username == self.password_1:
+            add_error_to_dict('password_1', errors_state, 'Password too weak')
+        if self.email == self.password_1:
+            add_error_to_dict('password_1', errors_state, 'Password too weak')
+        if False == is_password_strong(self.password_1):
+            add_error_to_dict('password_1', errors_state, 'Password too weak')
+            
+@dataclass
+class NewUserValidator:
+    username: str
+    email: str
+    
+    def validate(self, errors_state):
+        if User.objects.filter(email=self.email).exists():
+            add_error_to_dict('email', errors_state, 'User already exists.')
+        if User.objects.filter(username=self.username).exists():
+            add_error_to_dict('username', errors_state, 'User already exists.')
 
 class SignUpSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=100)
@@ -33,41 +71,49 @@ class SignUpSerializer(serializers.Serializer):
     password_2 = serializers.CharField(**PASSWORD_CHARFIELD_ARGS)
 
     def validate(self, data):
-        # explicitly check whether email and username are unique
-        if User.objects.filter(email=data['email']).exists():
-            raise serializers.ValidationError('User already exists.', code=status.HTTP_409_CONFLICT)
-        if User.objects.filter(username=data['username']).exists():
-            raise serializers.ValidationError('User already exists.', code=status.HTTP_409_CONFLICT)
-        if data['password_1'] != data['password_2']:
-            raise serializers.ValidationError('Passwords do not match', code=status.HTTP_400_BAD_REQUEST)
-        if data['username'] == data['password_1']:
-            raise serializers.ValidationError('Password too weak', code=status.HTTP_400_BAD_REQUEST)
-        password_policy_check(data['password_1'])
+        errors = {}
+        user_validator = NewUserValidator(
+            username=data['username'],
+            email=data['email']
+        )
+        user_validator.validate(errors)
+        password_validator = NewPasswordValidator(
+            username=data['username'],
+            email=data['email'],
+            password_1=data['password_1'],
+            password_2=data['password_2']
+        )
+        password_validator.validate(errors)
+        if errors:
+            raise serializers.ValidationError(errors)
         return data
     
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(**PASSWORD_CHARFIELD_ARGS)
     new_password_1 = serializers.CharField(**PASSWORD_CHARFIELD_ARGS)
     new_password_2 = serializers.CharField(**PASSWORD_CHARFIELD_ARGS)
-
-    def validate(self, data):
+    
+    def __get_user_object(self, data):
         try:
             authenticated_user = self.context.get('authenticated_user')
         except:
-            raise serializers.ValidationError('User do not exists.', code=status.HTTP_400_BAD_REQUEST)
-        username = authenticated_user.username
+            raise serializers.ValidationError('User do not exists.')
+        return authenticated_user
 
-        if data['new_password_1'] != data['new_password_2']:
-            raise serializers.ValidationError('Passwords do not match', code=status.HTTP_400_BAD_REQUEST)
-        if username == data['new_password_1']:
-            raise serializers.ValidationError('Password too weak', code=status.HTTP_400_BAD_REQUEST)
-        if data['old_password'] == data['new_password_1']:
-            raise serializers.ValidationError('Bad password', code=status.HTTP_400_BAD_REQUEST)
-        password_policy_check(data['new_password_1'])
-
-
-        if True == check_password(data['new_password_1'], authenticated_user.password):
-            raise serializers.ValidationError('Invalid password', code=status.HTTP_400_BAD_REQUEST)
+    def validate(self, data):
+        authenticated_user = self.__get_user_object(data)
         if False == check_password(data['old_password'], authenticated_user.password):
-            raise serializers.ValidationError('Invalid password', code=status.HTTP_400_BAD_REQUEST)
+            raise serializers.ValidationError({'odl_password' : "Invalid old password."})
+        username = authenticated_user.username
+        email = authenticated_user.email
+        errors = {}
+        password_validator = NewPasswordValidator(
+            username=username,
+            email=email,
+            password_1=data['new_password_1'],
+            password_2=data['new_password_2']
+        )
+        password_validator.validate(errors)
+        if errors:
+            raise serializers.ValidationError(errors)
         return data
